6c68fffcd7014192ae5f4e30b9879a92c21d06ab
Davidbg3 <dbarrera.4a.et@gmail.com>
2019-11-28 04:43:19 -0500

    Mejora enlace RecomendacionActivity y RecojoActivity

--- app/src/main/java/com/example/greencity/RecojoFragment.java
+++ app/src/main/java/com/example/greencity/RecojoFragment.java
@@ -1,14 +1,14 @@
 package com.example.greencity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
+import android.widget.TextView;
 
 
 public class RecojoFragment extends Fragment {
@@ -29,8 +29,22 @@ public class RecojoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recojo, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_recojo,
+                container, false);
+        TextView button = (TextView) rootView.findViewById(R.id.recojo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RecomendacionActivity.class);
+                startActivity(intent);
+            }
+        });
+        return rootView;
+
+
     }
+
+
 
 
 }
